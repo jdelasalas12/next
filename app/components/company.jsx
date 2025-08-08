ef7c830ad62db63071bee43bdd5d0b94b7d78bcd
jdelasalas12 <163302111+jdelasalas12@users.noreply.github.com>
2025-08-08 09:49:59 +0800

    add changes

--- app/components/company.jsx
+++ app/components/company.jsx
@@ -13,14 +13,14 @@ import {
 const Company = () => {
   return (
     <>
-      <div className="flex w-full max-w-[82rem] h-auto py-[100px] m-auto bg-[#F9F8FE] justify-center max-xl:flex-col max-xl:justify-center max-xl:content-center ">
-        <div className=" w-1/2 w-full h-auto mt-[auto] px-[40px]  max-xl:shrink max-xl:justify-center max-xl:m-10 sm:px-[auto] ">
-          <div className="flex w-auto h-auto flex-col">
+      <div className="flex w-full max-w-[82rem] h-auto py-[70px] m-auto bg-[#F9F8FE] justify-center max-xl:flex-col">
+        <div className=" w-1/2 w-full ml-[60] h-auto px-[40px] max-xl:mb-[50] max-xl:m-auto  ">
+          <div className="flex w-auto h-auto flex-col max-xl:justify-self-center">
             <h2 className="text-5xl font-semibold leading-[1.2] mb-[50] max-md:text-4xl ">
               How we support our <br />
               partner all over the world
             </h2>
-            <p className="text-[16px font-medium] mb-[50] text-[#A6A6A6] tracking-wide">
+            <p className="text-[16px font-medium] mb-[50] text-[#A6A6A6] tracking-wide leading-[2]">
               SaaS become a common delivery model for many business application,
               including
               <br /> office software, messaging software, payroll processing
@@ -28,19 +28,21 @@ const Company = () => {
               <br /> management software
             </p>
           </div>
-          <div className="flex gap-[100] ">
+          <div className="flex gap-[100] max-xl:justify-self-center max-sm:flex-wrap max-sm:gap-[20] max-sm:justify-center ">
             <div>
-              <div className="flex gap-[5] mb-[10] sm:mb-[5] ">
+              <div className="flex gap-[5] mr-[20] mb-[10] sm:mb-[5] max-sm:mr-[0] ">
                 <Image src={star} alt="" />
                 <Image src={star} alt="" />
                 <Image src={star} alt="" />
                 <Image src={star} alt="" />
                 <Image src={star} alt="" />
               </div>
-              <p className="text-[#191A15 text-[18px] mb-[10] sm:mb-[5]">
+              <p className="text-[#191A15 text-[18px] mb-[10] sm:mb-[5] max-sm:text-[14px] ">
                 4.9 / 5 rating
               </p>
-              <p className="text-[18px] text-[#A6A6A6]">databricks</p>
+              <p className="text-[18px] text-[#A6A6A6] max-sm:text-[12px]">
+                databricks
+              </p>
             </div>
             <div>
               <div className="flex gap-[5] mb-[10]">
@@ -50,16 +52,18 @@ const Company = () => {
                 <Image src={star} alt="" />
                 <Image src={startg} alt="" />
               </div>
-              <p className="text-[#191A15 text-[18px] mb-[10]">
+              <p className="text-[#191A15 text-[18px] mb-[10] max-sm:text-[14px]">
                 4.9 / 5 rating
               </p>
-              <p className="text-[18px] text-[#A6A6A6]">databricks</p>
+              <p className="text-[18px] text-[#A6A6A6] max-sm:text-[14px]">
+                databricks
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="w-2/5 flex  mr-[150] max-xl:shrink max-xl:m-auto max-xl:mt-[auto] sm:px-[auto]  sm:w-full ">
-          <div className="grid grid-cols-[0.3fr_1fr] gap-4 grid-rows-<1fr_1fr_1fr> gap-4">
+        <div className="w-1/2 flex  w-full mr-[20] px-[20px] max-xl:justify-center ">
+          <div className="grid grid-cols-[0.3fr_1fr] gap-8 grid-rows-<1fr_1fr_1fr> gap-8">
             <div className="flex justify-center">
               <div className="w-20 h-20 bg-[#FFFFFF] flex justify-center items-center">
                 <Image className="w-9 h-9" src={activity} alt="" />
@@ -67,7 +71,7 @@ const Company = () => {
             </div>
             <div>
               <h2 className="font-bold text-3xl max-md:text-2xl">Publishing</h2>
-              <p className="text-lg font-medium leading-[2] text-[#A6A6A6] tracking-wide">
+              <p className="text-lg font-medium leading-[2] text-[#A6A6A6] tracking-wide max-sm:text-xs">
                 Plan, collaborate, and publishing your content <br />
                 that drives meaningful engagement and growth <br />
                 for your brand
@@ -80,8 +84,8 @@ const Company = () => {
             </div>
             <div>
               <h2 className="font-bold text-3xl max-md:text-2xl">Analytics</h2>
-              <p className="text-lg font-medium leading-[2] text-[#A6A6A6] tracking-wide">
-                Analyze your performance and create goegeous
+              <p className="text-lg font-medium leading-[2] text-[#A6A6A6] tracking-wide max-sm:text-xs">
+                Analyze your performance and create gorgeous
                 <br /> report
               </p>
             </div>
@@ -92,7 +96,7 @@ const Company = () => {
             </div>
             <div>
               <h2 className="font-bold text-3xl max-md:text-2xl">Engagement</h2>
-              <p className="text-lg font-medium leading-[2] text-[#A6A6A6] tracking-wide">
+              <p className="text-lg font-medium leading-[2] text-[#A6A6A6] tracking-wide max-sm:text-xs">
                 Quickly navigate you anda engage with your adience
               </p>
             </div>
